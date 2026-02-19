5922137a5932b7502f6257355d810050bc4ea557
Shesher Hasan <shesehr0007@gmail.com>
2026-02-19 08:37:18 +0600

    chore: hydrate website from new templates

--- src/components/ui/SmoothScroll.tsx
+++ src/components/ui/SmoothScroll.tsx
@@ -5,7 +5,7 @@ export default function SmoothScroll() {
     useEffect(() => {
         const lenis = new Lenis({
             duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
             orientation: 'vertical',
             gestureOrientation: 'vertical',
             smoothWheel: true,
